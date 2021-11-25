e72f750de589684e6269924731da4e94aec0a3bb
Ibnuadha28 <adha.rohman@gmail.com>
2021-11-26 00:12:53 +0700

    Commit modul 3

---
+++ Modul3/app/src/main/java/id/ibnu_19104020/helloittelkompurwokerto/Halaman2Activity.kt
@@ -0,0 +1,11 @@
+package id.ibnu_19104020.helloittelkompurwokerto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
